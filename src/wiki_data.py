3f7ad1567807439f592280cfc6c1a089cb3c267a
sherinann <sherinannthomas1@gmail.com>
2017-05-31 18:39:43 +0530

    header and cloloring

--- src/wiki_data.py
+++ src/wiki_data.py
@@ -9,6 +9,15 @@ import ssl
 
 sys.tracebacklimit= None
 
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = "\033[1m"
+
+
 context=ssl._create_unverified_context()
 site="https://en.wikipedia.org/wiki/"
 
@@ -26,12 +35,24 @@ try:
     #print(soup.get_text)
 
 
-    stuff=soup.find('div',attrs={"class":"mw-content-ltr"}).find_all('p')
-    for element in stuff:
 
-        print(element.text)
-        print(" ")
-        print(" ")
+    tag_head=['h1','h2','h3','h4','h5','h6']
+    tag_para=['p']
+
+    tags= soup.findAll()
+    for tag in tags:
+        if tag.name in tag_head:
+            if tag.text=='References':
+                break
+            else:
+                print(OKBLUE+tag.text+ENDC)
+        elif tag.name in tag_para:
+            print(tag.text)
+        else:
+            continue
+
+
+
 
 except er.HTTPError as er1:
     print("no result")
