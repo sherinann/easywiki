ed9f584afecbc53770887ddf303a0e69b85153d8
sherinann <sherinannthomas1@gmail.com>
2017-06-01 21:25:47 +0530

    separated code into interface and function implementations

--- src/wiki_data.py
+++ src/wiki_data.py
@@ -1,14 +1,28 @@
 
 import urllib.request as re
 import urllib.error as er
-import sys
+
 
 
 from bs4 import BeautifulSoup
 import ssl
 
-class wiki:
-    search
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = "\033[1m"
+
+tag_head = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
+tag_para = ['p', 'li']
+
+
+
+context=ssl._create_unverified_context()
+
+
 
 def getSpecialResult(arg):
     searchQuery=arg.replace(" ", "+")
@@ -26,6 +40,7 @@ def getSpecialResult(arg):
 
 
 def getResult(site,searchItem):
+
 
     site_new = site + searchItem
 
@@ -49,46 +64,12 @@ def getResult(site,searchItem):
             else:
                 continue
     except er.HTTPError as er1:
-        getSpecialResult(sys.argv[1])
+        getSpecialResult(searchItem)
 
 
 
 
 
-sys.tracebacklimit= None
-
-HEADER = '\033[95m'
-OKBLUE = '\033[94m'
-OKGREEN = '\033[92m'
-WARNING = '\033[93m'
-FAIL = '\033[91m'
-ENDC = '\033[0m'
-BOLD = "\033[1m"
-
-tag_head = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
-tag_para = ['p', 'li']
-
-context=ssl._create_unverified_context()
-siteDefault="https://en.wikipedia.org/wiki/"
-
-search="https://en.wikipedia.org/w/index.php?search="+searchQuery+"&amp;title=Special%3ASearch&amp;fulltext=1"
 
 
-
-if __name__=="__main__":
-    try:
-
-        if len(sys.argv) > 2:
-            raise Exception( "too many arguments usage: wiki_data.py <arg1> \n[use \" \" to enclose argument with spaces]")
-        else:
-            site=siteDefault
-            searchItem=sys.argv[1]
-            getResult(site,searchItem)
-
-
-
-
-
-    except IndexError:
-        print("usage: wiki_data.py <arg1> \n[use \" \" to enclose argument with spaces]")
 
