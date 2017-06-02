2d3e9476f4492313f88794c9705af1057203c238
sherinann <sherinannthomas1@gmail.com>
2017-06-02 16:27:12 +0530

    removed unused code

--- setup.py
+++ setup.py
@@ -6,16 +6,15 @@ if not sys.version_info[0] == 3:
 
 setup(
     name='easywiki',
-    version='1.0.1dev',
+    version='1.0.2dev',
     packages=['easywiki'],
-    entry_points = {"console_scripts": ['easywiki = easywiki.__main__:main']},
-    install_requires=['bs4',],
     requires=['bs4',],
     url='https://github.com/sherinann/easywiki',
     license='GPL 3',
     author='Sherin Ann Thomas',
     author_email='sherinannthomas1@gmail.com',
     description='A simple CLI for using wikipedia from terminal',
-    download_url = 'https://github.com/sherinann/easywiki/archive/0.1.tar.gz'
+    download_url = 'https://github.com/sherinann/easywiki/archive/0.1.tar.gz',
+    scripts = ['bin/easywiki'],
 
 )
