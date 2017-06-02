02abc86e8b8598e9c9e72e541db86cb0e4375d8e
sherinann <sherinannthomas1@gmail.com>
2017-06-02 12:24:55 +0530

    setup.cfg and setup.py

--- setup.py
+++ setup.py
@@ -8,13 +8,14 @@ setup(
     name='easywiki',
     version='1.0.0dev',
     packages=['easywiki'],
-    entry_points = {"console_scripts": ['easywiki = easywiki.__main__:main']},
-    install_requires=['bs4',],
+    #entry_points = {"console_scripts": ['easywiki = easywiki.__main__:main']},
+    #install_requires=['bs4',],
     requires=['bs4',],
-    url='',
+    url='https://github.com/sherinann/easywiki',
     license='GPL 3',
     author='Sherin Ann Thomas',
     author_email='sherinannthomas1@gmail.com',
     description='A simple CLI for using wikipedia from terminal',
+    download_url = 'https://github.com/sherinann/easywiki/archive/0.1.tar.gz'
 
 )
